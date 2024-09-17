1908229723f7c13dc52e2a8f1405524088cf95b0
Muspi Merol <wjj123345567@icloud.com>
2024-09-17 20:53:23 +0800

    feat: proxy on the edge (vercel)

--- src/routes/nlark/[...path]/+server.ts
+++ src/routes/nlark/[...path]/+server.ts
@@ -4,3 +4,5 @@ export const GET: RequestHandler = async ({ fetch, params: { path } }) => {
   const res = await fetch(`https://cdn.nlark.com/${path}`);
   return new Response(res.body, res);
 };
+
+export const config = { runtime: "edge" };
