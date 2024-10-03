43fb4121977be87a70fb639f5f453acccc2bf29d
Muspi Merol <wjj123345567@icloud.com>
2024-10-03 17:25:28 +0800

    chore: move `DarkModeToggle` into a separated component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,11 @@
 <script>
   import Icon from "@iconify/svelte";
-  import { mode, toggleMode } from "mode-watcher";
-
-  let firstTime = true;
+  import DarkModeToggle from "$lib/DarkModeToggle.svelte";
 </script>
 
 <div class="h-full flex flex-row justify-center transition">
   <div class="flex flex-col justify-center gap-1 text-center text-teal-700 transition dark:text-teal-300">
-    <button class="mb-2.5 w-3.1em self-center rounded-full bg-zinc-100 p-0.3em text-xl transition ease-out active:scale-90 dark:bg-zinc-800 hover:bg-zinc-200 hover:dark:bg-zinc-700 dark:!bg-opacity-40" on:click={() => [(toggleMode()), (firstTime = false)]}>
-      <!-- container -> w - p = translate-x + w <- child -->
-      <div class="h-1.5em w-1.5em flex items-center justify-center rounded-full bg-white shadow-lg transition dark:translate-x-1em dark:bg-opacity-10 dark:shadow-none">
-        {#if $mode === "dark"} <Icon icon={firstTime ? "line-md:moon-loop" : "line-md:sunny-outline-to-moon-loop-transition"} /> {:else} <Icon icon={firstTime ? "line-md:sunny-outline" : "line-md:moon-to-sunny-outline-transition"} /> {/if}
-      </div>
-    </button>
+    <DarkModeToggle />
     <h1>Hi! I'm <strong>Muspi Merol</strong></h1>
     <h2 class="text-zinc-400"><a href="https://github.com/CNSeniorious000">@CNSeniorious000</a></h2>
 
@@ -36,11 +29,3 @@
     </h3>
   </div>
 </div>
-
-<style>
-  button,
-  button > div {
-    transition-timing-function: cubic-bezier(0, 0, 0, 1);
-    transition-duration: 0.3s;
-  }
-</style>
