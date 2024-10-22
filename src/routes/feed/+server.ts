72944c96735469a1d757a5d0d4012c408e8d2788
Muspi Merol <wjj123345567@icloud.com>
2024-10-22 13:40:54 +0800

    feat: inject full content into `/feed` endpoint

--- src/routes/feed/+server.ts
+++ src/routes/feed/+server.ts
@@ -1,22 +1,50 @@
 import type { RequestHandler } from "./$types";
 
 import { text } from "@sveltejs/kit";
+import { getPost } from "$lib/api/each";
 import { listPosts } from "$lib/api/list";
 import { escape } from "$lib/xml";
+import { load } from "cheerio";
+import { parseDocument } from "htmlparser2";
 
 export const GET: RequestHandler = async ({ url: { origin } }) => {
   const articles = await listPosts();
 
-  const entries = articles.map(({ id, slug, title, description, created_at, content_updated_at }) => `
-    <entry>
-      <id>${id}</id>
-      <title>${escape(title)}</title>
-      <link href="${origin}/blog/${slug}" />
-      <content src="${origin}/blog/${slug}" type="text/html" />
-      <summary>${escape(description)}</summary>
-      <published>${created_at}</published>
-      <updated>${content_updated_at}</updated>
-    </entry>`).join("\n");
+  const entries = (await Promise.all(articles.map(async ({ id, slug, title, description, created_at, content_updated_at }) => {
+    const { body_html } = await getPost(slug);
+
+    const $ = load(parseDocument(body_html));
+    $("img").each((_, img) => {
+      const src = $(img).attr("src");
+      if (src && src.startsWith("https://cdn.nlark.com/")) {
+        $(img).attr("src", src.replace("https://cdn.nlark.com/", `${origin}/nlark/`));
+      }
+    });
+    $("span").each(function () {
+      const content = $(this).html();
+      if (content) {
+        $(this).replaceWith(content);
+      }
+    });
+    for (const attribute of ["id", "class", "data-lake-index-type", "data-href"]) {
+      $(`[${attribute}]`).each((_, el) => {
+        $(el).removeAttr(attribute);
+      });
+    }
+
+    const content = $.html();
+
+    return `
+      <entry>
+        <id>${id}</id>
+        <title>${escape(title)}</title>
+        <link href="${origin}/blog/${slug}" />
+        <summary>${escape(description)}</summary>
+        <content type="html">${escape(content)}</content>
+        <published>${created_at}</published>
+        <updated>${content_updated_at}</updated>
+      </entry>`;
+  }))).join("\n");
 
   return text(`
     <?xml version="1.0" encoding="UTF-8" ?>
@@ -30,3 +58,5 @@ export const GET: RequestHandler = async ({ url: { origin } }) => {
     </feed>
   `.trim(), { headers: { "Content-Type": "application/xml; charset=utf-8" } });
 };
+
+export const config = { isr: { expiration: 10 } };
