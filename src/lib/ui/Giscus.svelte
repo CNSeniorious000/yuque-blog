7101d03fad1821d6d1fd46ae425a59253df868e7
Muspi Merol <wjj123345567@icloud.com>
2024-12-28 21:57:31 +0800

    feat: add giscus integration

---
+++ src/lib/ui/Giscus.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import Giscus from "@giscus/svelte";
+  import { dev } from "$app/environment";
+  import { mode } from "mode-watcher";
+
+  export let id: number;
+  export let strict = false;
+  export let reactionsEnabled = true;
+  export let emitMetadata = dev;
+  export let inputPosition: "top" | "bottom" = "top";
+  export let lang = "zh-CN";
+</script>
+
+<div class="m-6 transition-margin sm:m-10">
+  <Giscus
+    id="giscus"
+    repo="CNSeniorious000/yuque-blog"
+    repoId="R_kgDOImLjJQ"
+    category="giscus"
+    categoryId="DIC_kwDOImLjJc4Clj9D"
+    term={String(id)}
+    strict={strict ? "1" : "0"}
+    reactionsEnabled={reactionsEnabled ? "1" : "0"}
+    emitMetadata={emitMetadata ? "1" : "0"}
+    inputPosition={inputPosition}
+    theme="noborder_{$mode === "dark" ? "dark" : "light"}"
+    lang={lang}
+  />
+</div>
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -2,6 +2,7 @@
   import type { LayoutParentData, LayoutServerData } from "../$types";
 
   import Marked from "$lib/Marked.svelte";
+  import Giscus from "$lib/ui/Giscus.svelte";
   import { formatDate } from "$lib/utils";
   import Seo from "sk-seo";
 
@@ -10,9 +11,11 @@
 
 {#if data.article}
   <!-- a hack to keep these variables reactive -->
-  {@const { title, description, markdown, updated_at } = data.article}
+  {@const { id, title, description, markdown, updated_at } = data.article}
 
   <Marked {title} {description} {markdown} />
 
   <Seo {title} description={description || `最后更新于：${formatDate(updated_at, data.language)}`} />
+
+  <Giscus {id} lang={data.language} />
 {/if}
