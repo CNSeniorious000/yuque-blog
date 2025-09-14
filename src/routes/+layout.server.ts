288c36cd19b80c93a9cdde4b809300d35f97ec0d
Muspi Merol <me@promplate.dev>
2025-09-14 18:35:44 +0800

    feat: add a middleware to redirect AI bots' requests

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,20 @@
 import type { LayoutServerLoad } from "./$types";
 
-export const load = (async ({ cookies, request }) => {
+import { redirect } from "@sveltejs/kit";
+import { isAIBot } from "ua-parser-js/helpers";
+
+export const load = (async ({ cookies, request: { headers }, route, params }) => {
+  if (isAIBot(headers.get("user-agent") ?? "")) {
+    switch (route.id) {
+      case "/":
+        return redirect(302, "/llms-full.txt");
+      case "/blog":
+        return redirect(302, "/llms.txt");
+      case "/blog/[slug]":
+        return redirect(302, `/blog/${params.slug}/llms.txt`);
+    }
+  }
   const mode = cookies.get("mode") as "dark" | "light" | undefined;
-  const language = request.headers.get("accept-language")?.split(",")[0];
+  const language = headers.get("accept-language")?.split(",")[0];
   return { mode, language };
 }) satisfies LayoutServerLoad;
