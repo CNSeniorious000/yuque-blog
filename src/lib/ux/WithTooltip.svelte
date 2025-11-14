14e50ae5d671411b46adb0efbdac253bec5e3901
Muspi Merol <me@promplate.dev>
2025-11-14 19:16:19 +0800

    fix: ensure outer transition for tooltips

--- src/lib/ux/WithTooltip.svelte
+++ src/lib/ux/WithTooltip.svelte
@@ -28,9 +28,11 @@
       {@render children?.({ props: { ...props, onfocus: show, onblur: hide, onmouseenter: show, onmouseleave: hide } })}
     {/snippet}
   </Popover.Trigger>
-  <Popover.Content sideOffset={4} collisionPadding={4} trapFocus={false} side="top" onOpenAutoFocus={e => e.preventDefault()} onCloseAutoFocus={e => e.preventDefault()}>
-    <div transition:fly|global={{ y: 2, duration: 300 }} class="rounded bg-white px-0.5em py-0.3em text-xs text-zinc-7 shadow shadow-zinc-7/10 backdrop-blur transition-colors dark:(bg-zinc-7/40 text-zinc-3)">
-      <InlineMarkdown markdown={tips} />
-    </div>
+  <Popover.Content forceMount sideOffset={4} collisionPadding={4} trapFocus={false} side="top">
+    {#if open}
+      <div transition:fly|global={{ y: 2, duration: 300 }} class="rounded bg-white px-0.5em py-0.3em text-xs text-zinc-7 shadow shadow-zinc-7/10 backdrop-blur transition-colors dark:(bg-zinc-7/40 text-zinc-3)">
+        <InlineMarkdown markdown={tips} />
+      </div>
+    {/if}
   </Popover.Content>
 </Popover.Root>
