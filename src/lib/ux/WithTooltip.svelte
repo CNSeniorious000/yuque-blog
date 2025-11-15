435dcdb63a1abca017919973f988484b8f9378b9
Muspi Merol <me@promplate.dev>
2025-11-16 00:03:06 +0800

    fix: disable the default auto focusing behavior for `WithTooltip`

--- src/lib/ux/WithTooltip.svelte
+++ src/lib/ux/WithTooltip.svelte
@@ -28,7 +28,7 @@
       {@render children?.({ props: { ...props, onfocus: show, onblur: hide, onmouseenter: show, onmouseleave: hide } })}
     {/snippet}
   </Popover.Trigger>
-  <Popover.Content forceMount sideOffset={4} collisionPadding={4} trapFocus={false} side="top">
+  <Popover.Content forceMount sideOffset={4} collisionPadding={4} trapFocus={false} side="top" onOpenAutoFocus={e => e.preventDefault()} onCloseAutoFocus={e => e.preventDefault()}>
     {#if open}
       <div transition:fly|global={{ y: 2, duration: 300 }} class="rounded bg-white px-0.5em py-0.3em text-xs text-zinc-7 shadow shadow-zinc-7/10 backdrop-blur transition-colors dark:(bg-zinc-7/40 text-zinc-3)">
         <InlineMarkdown markdown={tips} />
